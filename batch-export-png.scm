a9f85409bd4b77e567d9d7d857803146cb6e19d5
Matti J. Kärki <mjk@iki.fi>
2019-02-19 10:26:44 +0200

    Added batch-export-png.scm script
    Updated script description to README.md

---
+++ batch-export-png.scm
@@ -0,0 +1,30 @@
+(script-fu-register
+ "script-fu-batch-export-png"
+ "Export All Images to PNG"
+ "Exports all open images to PNG files."
+ "Matti J. Kärki <mjk@iki.fi>"
+ "Copyright 2019, Matti J. Kärki"
+ "2019-02-19"
+ "RGB*"
+ SF-DIRNAME "Folder" "")
+
+(script-fu-menu-register
+ "script-fu-batch-export-png"
+ "<Image>/Image")
+
+(define (script-fu-batch-export-png folder)
+  (for-each (lambda (img)
+              (let ((filename (string-append folder "\\image_" (number->string img) ".png")))
+                (file-png-save RUN-NONINTERACTIVE
+                               img
+                               (car (gimp-image-get-active-drawable img))
+                               filename
+                               filename
+                               0
+                               6
+                               0
+                               0
+                               0
+                               0
+                               0)))
+            (vector->list (cadr (gimp-image-list)))))
